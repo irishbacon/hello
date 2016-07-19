01b52b3ac33f3d8d512507130c74beb85793bade
Kyle <kyle@ionic.com>
2016-07-19 14:20:58 -0400

    other stuff

--- hello.go
+++ hello.go
@@ -1,11 +1,52 @@
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/irishbacon/stringutil"
+// )
+
+// func main() {
+// 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+// }
+
+
+// fibonacci is a function that returns
+// a function that returns an int.
+// func fibonacci() func() int {
+// 	a, b := 0, 1
+// 	return func() (ret int) {
+// 		ret, a, b = a, b, a+b
+// 		return
+// 	}
+// }
+
+// func main() {
+// 	f := fibonacci()
+// 	for i := 0; i < 10; i++ {
+// 		fmt.Println(f())
+// 	}
+// }
+
+
 package main
 
-import (
-	"fmt"
+import "golang.org/x/tour/pic"
 
-	"github.com/irishbacon/stringutil"
-)
+func Pic(dx, dy int) [][]uint8 {
+ var xs = make([]([]uint8), dy)
+    for y, _ := range xs {
+        xs[y] = make([]uint8, dx)
+        for x, _ := range xs[y] {
+            //xs[y][x] = uint8((x & y) & (x & y))
+			xs[y][x] = uint8((x * y) & ( x * y))
+        }
+    }
+    
+  return xs  
+
+}
 
 func main() {
-	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+    pic.Show(Pic)
 }
