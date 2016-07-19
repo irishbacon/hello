835bf01a8349b9b6798852fe7c3560e63f138eb4
Kyle <kyle@ionic.com>
2016-07-19 14:38:15 -0400

    fibonnaci

--- hello.go
+++ hello.go
@@ -1,52 +1,52 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
+import (
+	"fmt"
 
 // 	"github.com/irishbacon/stringutil"
-// )
+)
 
 // func main() {
 // 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 // }
 
-
 // fibonacci is a function that returns
 // a function that returns an int.
-// func fibonacci() func() int {
-// 	a, b := 0, 1
-// 	return func() (ret int) {
-// 		ret, a, b = a, b, a+b
-// 		return
-// 	}
-// }
-
-// func main() {
-// 	f := fibonacci()
-// 	for i := 0; i < 10; i++ {
-// 		fmt.Println(f())
-// 	}
-// }
-
-
-package main
-
-import "golang.org/x/tour/pic"
-
-func Pic(dx, dy int) [][]uint8 {
- var xs = make([]([]uint8), dy)
-    for y, _ := range xs {
-        xs[y] = make([]uint8, dx)
-        for x, _ := range xs[y] {
-            //xs[y][x] = uint8((x & y) & (x & y))
-			xs[y][x] = uint8((x * y) & ( x * y))
-        }
-    }
-    
-  return xs  
-
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() (ret int) {
+		ret, a, b = a, b, a+b
+		return
+	}
 }
 
 func main() {
-    pic.Show(Pic)
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
 }
+
+
+
+// package main
+
+// import "golang.org/x/tour/pic"
+
+// func Pic(dx, dy int) [][]uint8 {
+//  var xs = make([]([]uint8), dy)
+//     for y, _ := range xs {
+//         xs[y] = make([]uint8, dx)
+//         for x, _ := range xs[y] {
+//             //xs[y][x] = uint8((x & y) & (x & y))
+// 			xs[y][x] = uint8((x * y) & ( x * y))
+//         }
+//     }
+    
+//   return xs  
+
+// }
+
+// func main() {
+//     pic.Show(Pic)
+// }
